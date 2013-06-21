374b1a42f4f03504be2a7e10bf9154cbf84f3d30
Justin Haight <haight@gmail.com>
2013-06-21 14:47:52 -0700

    RailsCast #405 verbatem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raffler::Application.config.secret_token = '6048e990edb65fbd6c9dd0ca10b0c3ed77a506a053a49ad4784dec1805913fa99b0db5be501e969bddaa7c44be1c3d3b5b059a52622bb76183f217a4488cca69'
